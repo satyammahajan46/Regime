ba179259d58ffdeb68632b44f72c14dc61cd714b
satyammahajan46 <satyammahajan46@gmail.com>
2018-10-30 13:59:22 -0700

    Some Fixes and login system
    Bugs and Crashes fix.
    Login System Implemented

--- commonHeader.php
+++ commonHeader.php
@@ -12,7 +12,7 @@ include 'header_files.php';
                     <div class="col-md-8">
                         <ul class="nav navbar-nav navbar-right cl-effect-5">
                         <li class="active"><a href="index.php" class="page-scroll"><span data-hover="Home">Home</span></a></li>
-                        <li><a href="test.php" class="page-scroll"><span data-hover="Sign&nbsp;Up">Sign Up</span></a></li>
+                        <li><a href="signup.php" class="page-scroll"><span data-hover="Sign&nbsp;Up">Sign Up</span></a></li>
                         <li><a href="login.php" class="page-scroll"><span data-hover="Login">Login</span></a></li>
                         <li><a href="test.php" class="page-scroll"><span data-hover="Contact&nbsp;Us">Contact Us</span></a></li>
                         </ul>
