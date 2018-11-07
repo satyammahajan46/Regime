d92d13f4fcdf2955222e0555fe4e6245611566fa
satyammahajan46 <satyammahajan46@gmail.com>
2018-11-07 12:08:36 -0800

    Update printTable.php

---
+++ printTable.php
@@ -0,0 +1,15 @@
+<?php
+include "connection.php"
+    $select = "Select b.bname, b.bedition, b.bprice from `book price` b"
+    if(isset($selected){
+        include "table.php"
+?>
+<table class="table table-bordered table-hover">
+    <tr>
+        <th colspan="3"><h3 class="text-center">Book Found were</h3></th>
+    </tr>
+    <tr>
+        <th>Book Name</th>
+    </tr>
+    <?php
+    }?>
