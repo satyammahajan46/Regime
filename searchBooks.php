495225f87143cedd179b771ae51659c0e35a6ede
Paul Lucas <paul.lucas8@gmail.com>
2018-11-13 11:26:37 -0800

    Added Authors, BName

--- searchBooks.php
+++ searchBooks.php
@@ -6,10 +6,11 @@
     // output data of each row
     	echo '<form action="search.php" method="post">';
 
+        echo '<table><th>';
         while($row = $result->fetch_assoc()) {  
-            echo '<input type="checkbox" name="genre_list[]" value='.$row["BGenre"]. '><label> &nbsp'.$row["BGenre"].'</label> ';
+            echo '<tr><td><input type="checkbox" name="genre_list[]" value='.$row["BGenre"]. '><label> &nbsp'.$row["BGenre"].'</label> </td></tr>';
         }
-        echo '<br><input type="submit" name="submit" value="Submit"/> </form>';
+        echo '</table><br><input type="submit" name="submit" value="Submit"/> </form>';
     } else {
         echo "No Books Found";
     }
@@ -21,23 +22,24 @@
 			$string = "";
 			foreach($_POST['genre_list'] as $selected){
 				if (count($_POST['genre_list']) - 1 == $num ){
-					$string .= " BGenre = " ."'" .$selected."'";
+					$string .= " bg.BGenre = " ."'" .$selected."'";
 				}else{
-					$string .= " BGenre = " . "'" .$selected ."' OR ";
+					$string .= " bg.BGenre = " . "'" .$selected ."' OR ";
 					$num++;
 				}
 			}
-			$query2 = "SELECT BISBN FROM `book genre` WHERE" . $string;
+			$query2 = "SELECT bn.BName, a.AName FROM `book name` bn, `book genre` bg, `authors` a, `writes` w WHERE bn.BISBN = bg.BISBN AND w.BID = bn.BID AND a.AID = w.AID AND (" . $string.")";
 			$result2 = mysqli_query($conn,$query2);
 
 		    if ($result2->num_rows > 0) {
 		    // output data of each row
-		    	echo '<table align="center" border= "1">';
+		    	echo '<table border= "1">';
 				echo '<tr>
-				<th>BISBN</th>
+				<th>Book Name</th>
+				<th>Author</th>
 				</tr>';
 		        while($row2 = $result2->fetch_assoc()) {  
-		            echo "<tr> <th>". $row2["BISBN"]. "</th> </tr> ";
+		            echo "<tr> <th>". $row2["BName"]. "</th><th>". $row2["AName"]. "</th></tr> ";
 		        }
 		    } else {
 		        echo "No Books Found ";
