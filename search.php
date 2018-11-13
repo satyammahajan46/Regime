3e23085bbca4ff520d6bcb3df71941afde27bdb4
satyammahajan46 <satyammahajan46@gmail.com>
2018-11-13 13:21:18 -0800

    Fixed ALOT OF BUGS

--- search.php
+++ search.php
@@ -1,36 +1,71 @@
 <?php
-    include "validateLogin.php";
-    ?>
-<html>
-<head>
-<title>Login</title>
-<?php
-    include "mainHeader.php";
-    ?>
-<title>Welcome</title>
-</head>
-<body>
-<div>
-<?php
-    include "connection.php";
-    ?>
-
-
-<div class="container">
-<h1 class="center-block">Genre</h1>
-<?php
-    include "searchBooks.php";
+include "validateLogin.php";
 ?>
-</div>
-</div>
+<html>
+    <head>
+        <title>Login</title>
+        <?php
+        include "mainHeader.php";
+        ?>
+        <title>Welcome</title>
+    </head>
+    <body>
 
-<footer>
-<section class="copyright-w3-agileits">
-<?php
-    include "footer.php";
-    ?>
-</section>
-</footer>
-</body>
+        <?php
+        include "connection.php";
+        ?>
+
+        <?php
+        if(isset($_GET["s"])){
+            include "printTable.php";
+
+        }
+        ?>
+
+
+        <div class="container">
+
+            <?php
+
+
+            $query = "SELECT DISTINCT BGenre FROM `book genre`";
+            $result = mysqli_query($conn,$query);
+
+            if ($result->num_rows > 0) {
+                // output data of each row
+                echo '<h1 class="center-block">Genre</h1>';
+                echo '<form action="search.php?s=1" method="post">';
+
+                echo '<table><th>';
+                while($row = $result->fetch_assoc()) {  
+                    echo '<td><input type="checkbox" name="genre_list[]" value='.$row["BGenre"]. '><label> &nbsp'.$row["BGenre"].'</label> &nbsp </td>';
+
+                }
+                echo '</td></table><br><input type="submit" name="submit" value="Submit"/> </form>';
+            } else {
+                echo "No Books Found";
+            }
+
+            if(isset($_GET["s"])){
+                echo '<h1 class="center-block">Books</h1>';
+                echo $final;
+            }
+            ?>
+
+
+        </div>
+
+
+        <footer style="padding-top:50px">
+            <section class="copyright-w3-agileits">
+                <?php
+                include "footer.php";
+                ?>
+            </section>
+        </footer>
+
+    </body>
+
+
 </html>
 
--- searchBooks.php
+++ searchBooks.php
@@ -1,53 +1,27 @@
 <?php
-	$query = "SELECT DISTINCT BGenre FROM `book genre`";
-    $result = mysqli_query($conn,$query);
-
-    if ($result->num_rows > 0) {
-    // output data of each row
-    	echo '<form action="search.php" method="post">';
-
-        echo '<table><th>';
-        while($row = $result->fetch_assoc()) {  
-            echo '<tr><td><input type="checkbox" name="genre_list[]" value='.$row["BGenre"]. '><label> &nbsp'.$row["BGenre"].'</label> </td></tr>';
-        }
-        echo '</table><br><input type="submit" name="submit" value="Submit"/> </form>';
-    } else {
-        echo "No Books Found";
-    }
-
-  
-    if(isset($_POST['submit'])){
-		if(!empty($_POST['genre_list'])){
-			$num = 0;
-			$string = "";
-			foreach($_POST['genre_list'] as $selected){
-				if (count($_POST['genre_list']) - 1 == $num ){
-					$string .= " bg.BGenre = " ."'" .$selected."'";
-				}else{
-					$string .= " bg.BGenre = " . "'" .$selected ."' OR ";
-					$num++;
-				}
-			}
-			$query2 = "SELECT bn.BName, a.AName FROM `book name` bn, `book genre` bg, `authors` a, `writes` w WHERE bn.BISBN = bg.BISBN AND w.BID = bn.BID AND a.AID = w.AID AND (" . $string.")";
-			$result2 = mysqli_query($conn,$query2);
-
-		    if ($result2->num_rows > 0) {
-		    // output data of each row
-		    	echo '<table border= "1">';
-				echo '<tr>
-				<th>Book Name</th>
-				<th>Author</th>
-				</tr>';
-		        while($row2 = $result2->fetch_assoc()) {  
-		            echo "<tr> <th>". $row2["BName"]. "</th><th>". $row2["AName"]. "</th></tr> ";
-		        }
-		    } else {
-		        echo "No Books Found ";
-		    }
-
-		}
-		
-	}
-
-
+include "validateLogin.php";
 ?>
+<html>
+    <head>
+        <title>Login</title>
+        <?php
+        include "mainHeader.php";
+        ?>
+        <title>Welcome</title>
+    </head>
+    <body>
+        <div class="container">
+            <?php
+            include "connection.php";
+            include "printTable.php";
+            ?>
+        </div>
+        <footer style="padding-top:50px">
+            <section class="copyright-w3-agileits">
+                <?php
+                include "footer.php";
+                ?>
+            </section>
+        </footer>
+    </body>
+</html>
