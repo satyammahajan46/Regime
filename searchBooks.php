fbc29389ed98c600aedf19985d73dfda0e29bb10
Paul Lucas <paul.lucas8@gmail.com>
2018-11-13 12:31:30 -0800

    added buy feature

--- searchBooks.php
+++ searchBooks.php
@@ -8,7 +8,7 @@
 
         echo '<table><th>';
         while($row = $result->fetch_assoc()) {  
-            echo '<tr><td><input type="checkbox" name="genre_list[]" value='.$row["BGenre"]. '><label> &nbsp'.$row["BGenre"].'</label> </td></tr>';
+            echo '<tr><td><input type="checkbox" name="genre_list[]" value="'.$row["BGenre"]. '""><label> &nbsp'.$row["BGenre"].'</label> </td></tr>';
         }
         echo '</table><br><input type="submit" name="submit" value="Submit"/> </form>';
     } else {
@@ -28,19 +28,23 @@
 					$num++;
 				}
 			}
-			$query2 = "SELECT bn.BName, a.AName FROM `book name` bn, `book genre` bg, `authors` a, `writes` w WHERE bn.BISBN = bg.BISBN AND w.BID = bn.BID AND a.AID = w.AID AND (" . $string.")";
+			$query2 = "SELECT bn.BName, a.AName, bn.BID FROM `book name` bn, `book genre` bg, `authors` a, `writes` w WHERE bn.BISBN = bg.BISBN AND w.BID = bn.BID AND a.AID = w.AID AND (" . $string.")";
 			$result2 = mysqli_query($conn,$query2);
 
 		    if ($result2->num_rows > 0) {
 		    // output data of each row
+		    	echo '<form action="buy.php" method="post">';
 		    	echo '<table border= "1">';
 				echo '<tr>
 				<th>Book Name</th>
 				<th>Author</th>
+				<th>Purchase</th>
 				</tr>';
 		        while($row2 = $result2->fetch_assoc()) {  
-		            echo "<tr> <th>". $row2["BName"]. "</th><th>". $row2["AName"]. "</th></tr> ";
+		            echo "<tr><th>". $row2["BName"]. "</th><th>". $row2["AName"].
+		            "</th><th><button type='submit' name='buy' value='". $row2["BID"]."' >Buy</button></tr>";
 		        }
+		        echo "</form>";
 		    } else {
 		        echo "No Books Found ";
 		    }
@@ -48,6 +52,4 @@
 		}
 		
 	}
-
-
 ?>
