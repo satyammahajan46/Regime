766f92071b884dd3d0744b0ef69a511c313e52a6
satyammahajan46 <satyammahajan46@gmail.com>
2018-11-14 11:29:05 -0800

    Fixes

--- printTable.php
+++ printTable.php
@@ -1,6 +1,6 @@
 
 <?php
-if(isset($_POST['submit'])){
+/*if(isset($_POST['submit'])){
     if(!empty($_POST['genre_list'])){
         $num = 0;
         $string = "";
@@ -32,6 +32,48 @@ if(isset($_POST['submit'])){
 
     }
 
-}
+}*/
 ?>
 
+<?php
+if(isset($_POST['submit'])){
+    if(!empty($_POST['genre_list'])){
+        $num = 0;
+        $string = "";
+        foreach($_POST['genre_list'] as $selected){
+            if (count($_POST['genre_list']) - 1 == $num ){
+                $string .= " bg.BGenre = " ."'" .$selected."'";
+            }else{
+                $string .= " bg.BGenre = " . "'" .$selected ."' OR ";
+                $num++;
+            }
+        }
+        $query2 = "SELECT bn.BName, a.AName, bn.BID FROM `book name` bn, `book genre` bg, `authors` a, `writes` w WHERE bn.BISBN = bg.BISBN AND w.BID = bn.BID AND a.AID = w.AID AND (" . $string.")";
+        $result2 = mysqli_query($conn,$query2);
+
+        if ($result2->num_rows > 0) {
+            // output data of each row
+            echo '<h1 class="center-block">Books</h1>';
+            echo '<form action="buy.php" method="post">';
+            echo '<table class="table table-bordered table hover">';
+            echo '<tr>
+    <th>Book Name</th>
+    <th>Author</th>
+    <th>Purchase</th>
+    </tr>';
+            while($row2 = $result2->fetch_assoc()) {  
+                echo "<tr><th>". $row2["BName"]. "</th><th>". $row2["AName"].
+                    "</th><th><button type='submit' name='buy' value='". $row2["BID"]."' >Buy</button></tr>";
+            }
+            echo "</table></form>";
+
+        } else {
+            echo "No Books Found ";
+        }
+
+    }
+
+}
+
+
+?>
