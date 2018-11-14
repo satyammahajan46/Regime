a4b4daaf1aa971824697fdbed7f68b4cf2b712f6
Paul Lucas <paul.lucas8@gmail.com>
2018-11-14 12:07:31 -0800

    Updated Queries

--- crate.php
+++ crate.php
@@ -6,7 +6,13 @@
     $UIDget = mysqli_query($conn,$UIDquery);
     $UIDrow = $UIDget->fetch_assoc();
 
-    $select = "SELECT bn.BID, bn.BName, bp.BEdition, bp.BPrice FROM `book name` bn, `book price` bp, `buys` b  WHERE bn.BName=bp.BName AND b.UID =".$UIDrow["UID"]." AND b.BID = bn.BID";
+    $select = "SELECT bn.BID, bn.BName, bp.BEdition, bp.BPrice FROM `book name` bn, `book price` bp, `book key` bk, `buys` b WHERE 
+        bn.BName=bp.BName AND 
+        bp.BEdition = bk.BEdition AND 
+        bp.BName = bn.BName AND 
+        bn.BID = bk.BID AND 
+        b.BID = bn.BID AND 
+        b.UID =".$UIDrow["UID"];
     $result = $conn->query($select);
 
     if ($result){
@@ -14,7 +20,7 @@
             echo "<br>";
             echo "id: " . $row["BID"]. " - Name: " . $row["BName"]. " " . $row["BPrice"]. "<br>";
         }
-        
+
     } else {
         echo "0 results";
     }
