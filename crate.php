bf895d97199f7ab7ff1811b2f44b18e892777a6d
Paul Lucas <paul.lucas8@gmail.com>
2018-11-14 11:33:54 -0800

    update crate

--- crate.php
+++ crate.php
@@ -1,11 +1,17 @@
 <?php
     
     include "connection.php";
-    $select = "SELECT bn.BID, bn.BName, bp.BEdition, bp.BPrice FROM `book name` bn, `book price` bp WHERE bn.BName=bp.BName";
+    $user = $_SESSION["UEmail"];
+    $UIDquery = "SELECT UID FROM `user login` WHERE UEmail = '".$user."'";
+    $UIDget = mysqli_query($conn,$UIDquery);
+    $UIDrow = $UIDget->fetch_assoc();
+
+    $select = "SELECT bn.BID, bn.BName, bp.BEdition, bp.BPrice FROM `book name` bn, `book price` bp, `buys` b  WHERE bn.BName=bp.BName AND b.UID =".$UIDrow["UID"]." AND b.BID = bn.BID";
     $result = $conn->query($select);
 
     if ($result){
         while($row = $result->fetch_assoc()) {
+            echo "<br>";
             echo "id: " . $row["BID"]. " - Name: " . $row["BName"]. " " . $row["BPrice"]. "<br>";
         }
     } else {
